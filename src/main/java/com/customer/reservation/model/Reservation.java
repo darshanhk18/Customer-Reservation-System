0cce4de34755120569c6cb732abbbeb5cf588e19
KL34TG <darshan.h.kk.ext@ing.com>
2023-01-17 18:32:40 +0530

    Commit

--- src/main/java/com/customer/reservation/model/Reservation.java
+++ src/main/java/com/customer/reservation/model/Reservation.java
@@ -1,8 +1,18 @@
 package com.customer.reservation.model;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class Reservation {
 
   private String status;
+  private Integer id;
+  private Time startTime;
+  private String day;
+  private Date date;
+  private Integer duration;
+  private Integer retired;
+  private Integer empid;
 
 
 }
